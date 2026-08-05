4f1d281fb9078fcbc08616a46bcca56cf04cf06f
Isa <isadorasilva.lessa@gmail.com>
2026-08-04 22:19:19 -0300

    fim-criaçao-clientes-reservas

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express"
+import clientes from './router/clientes.js'
 
 const app = express()
 app.use(express.json)
-
+app.use(clientes)
 
 app.listen(3000, () => {
     console.log("Servidor Rodando")
