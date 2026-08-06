aa074dbf8ba564c0cdc5bc1a2f9ef57fc9442112
saulo <saulosouzalemos@gmail.com>
2026-08-06 20:06:46 -0300

    commit 2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import bebidasRouter from './router/bebidas.js';
+import express from 'express'
+import bebidasRouter from './router/bebidas.js'
 
-const app = express();
-
-
-app.use(express.json());
+const app = express()
 
 
-app.use('/bebidas', bebidasRouter);
+app.use(express.json())
+
+
+app.use('/bebidas', bebidasRouter)
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+  console.log('Servidor rodando na porta 3000')
+})
